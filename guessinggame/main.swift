3ee193e7b8f58064112afd384017bbb5a56a0afb
Thomas Dimeny <thomasdimeny@Interapts-MacBook-Pro.local>
2018-01-23 15:44:33 -0500

    first commit

---
+++ guessinggame/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  guessinggame
+//
+//  Created by Thomas Dimeny on 1/23/18.
+//  Copyright © 2018 Thomas Dimeny. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
